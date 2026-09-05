f22b081788517019586cca0f3f3499d2f0169838
KARAN GOND <karankumargond346@gmail.com>
2026-09-05 08:49:23 +0530

    Add click event to display success message

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('clickBtn').addEventListener('click', function() {
+    document.getElementById('message').innerText = "Congratulations! Your first webpage is successfully connected with HTML, CSS, and JavaScript.";
+});
